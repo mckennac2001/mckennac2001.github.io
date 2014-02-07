5042ffac51be7ace126659dc09f3c9efffaa3081
mckennac2001 <mckenna.charles@gmail.com>
2014-02-07 16:05:08 +0000

    maps, targets, index and game page

---
+++ js/map.js
@@ -0,0 +1,13 @@
+
+function detectBrowser() {
+  var useragent = navigator.userAgent;
+  var mapdiv = document.getElementById("map-canvas");
+
+  if (useragent.indexOf('iPhone') != -1 || useragent.indexOf('Android') != -1 ) {
+    mapdiv.style.width = '100%';
+    mapdiv.style.height = '100%';
+  } else {
+    mapdiv.style.width = '600px';
+    mapdiv.style.height = '800px';
+  }
+}
